78f615643faf85e038139287f6c743efdc4752ad
GIANCARLOS RENZO <124851283+Giancarlos1024@users.noreply.github.com>
2025-05-28 00:02:01 -0500

    Add files via upload

---
+++ SQLQuery_AppSolicitudes.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE SolicitudesDB;
+GO
+USE SolicitudesDB;
+GO
+CREATE TABLE Usuarios (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    NombreUsuario NVARCHAR(50) NOT NULL UNIQUE,
+    Contrasena NVARCHAR(100) NOT NULL
+);
+CREATE TABLE Solicitudes (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    FechaRegistro DATETIME NOT NULL DEFAULT GETDATE(),
+    UsuarioRegistro NVARCHAR(50),
+    CodigoSolicitud NVARCHAR(50) UNIQUE,
+    Detalle NVARCHAR(MAX),
+    Modificado BIT DEFAULT 0,
+    Eliminado BIT DEFAULT 0
+);
+
+CREATE TABLE Eliminaciones (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    FechaEliminacion DATETIME NOT NULL DEFAULT GETDATE(),
+    Motivo NVARCHAR(MAX),
+    CodigoSolicitud NVARCHAR(50),
+    UsuarioSolicitante NVARCHAR(50),
+    UsuarioEliminador NVARCHAR(50),
+    Detalle NVARCHAR(MAX)
+);
+INSERT INTO Usuarios (NombreUsuario, Contrasena)
+VALUES ('admin', 'admin1024*');
+
+SELECT * FROM Usuarios;
+SELECT * FROM Solicitudes;
+
+SELECT * FROM Eliminaciones
+
